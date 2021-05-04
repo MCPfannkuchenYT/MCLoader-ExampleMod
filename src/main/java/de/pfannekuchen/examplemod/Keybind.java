0a22568beaefbec5a1b42447df80b57f6c952fda
Pfannekuchen <gamesupport@mgnet.work>
2021-05-04 16:40:02 +0200

    Document the Code

--- src/main/java/de/pfannekuchen/examplemod/Keybind.java
+++ src/main/java/de/pfannekuchen/examplemod/Keybind.java
@@ -5,39 +5,34 @@ import java.util.Arrays;
 
 import de.pfannekuchen.mcloader.ReflectionHelper;
 
+/**
+ * Since you don't have access to Minecraft Classes, you need to recreate them using Reflection and Javassist.
+ * Here is such an example with a Keybind.
+ * @author Pancake
+ */
 public class Keybind {
-
-	public Object self;
-	String name;
-	int keycode;
-	String category;
 	
+	/**
+	 * This is where the magic happens. Here, we are creating
+	 * a new Keybind using Reflection, and then put it into the Array of all Keybinds in GameSettings.
+	 * @author Pancake
+	 */
 	public Keybind(String name, int keycode, String category) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, SecurityException, ClassNotFoundException, NoSuchFieldException {
-		this.name = name;
-		this.keycode = keycode;
-		this.category = category;
-		self = Class.forName("bhy").getConstructors()[0].newInstance(name, keycode, category);
+		/* Create a new Keybind */
+		Object self = Class.forName("bhy").getConstructors()[0].newInstance(name, keycode, category); // Use the first Constructor which takes 3 arguments. These being the Name, Keycode and Category of the Keybind.
 		
-		final Object minecraftClient = ReflectionHelper.getStaticInstance(ReflectionHelper.getDeclaredField("bib", "R"));
-		Object gameSettings = ReflectionHelper.getField("bib", "t").get(minecraftClient);
-		Object[] keybindArray = (Object[]) ReflectionHelper.getField("bid", "as").get(gameSettings);
+		/* Obtain the Keybind Array */
+		final Object minecraftClient = ReflectionHelper.getStaticInstance(ReflectionHelper.getDeclaredField("bib", "R")); // Get Minecraft from the Private Singleton(R) in the Minecraft(bib) Class.
+		Object gameSettings = ReflectionHelper.getField("bib", "t").get(minecraftClient); // Get the GameSettings from Minecraft(bib) which are saved in gameSettings(t)
+		Object[] keybindArray = (Object[]) ReflectionHelper.getField("bid", "as").get(gameSettings); // Get the Array with all Keybinds from the GameSettings(bid) which is saved in keybinds(as)
 		
+		/* Extend the Array and add the Keybind to it */
 		int N = keybindArray.length;
-		keybindArray = Arrays.copyOf(keybindArray, N + 1);
-		keybindArray[N] = self;
-		ReflectionHelper.getField("bid", "as").set(gameSettings, keybindArray);
-	}
-	
-	public String getName() {
-		return name;
-	}
-
-	public int getKeycode() {
-		return keycode;
-	}
-
-	public String getCategory() {
-		return category;
+		keybindArray = Arrays.copyOf(keybindArray, N + 1); // Copy Array and set length 1 more.
+		keybindArray[N] = self; // Set the last index to the Keybind
+		
+		/* Reinsert the new Keybind Array */
+		ReflectionHelper.getField("bid", "as").set(gameSettings, keybindArray); // Put the Copy of the Array to keybinds(as) in GameSettings(bid)
 	}
 	
 }
--- src/main/java/entry/EntryPoint.java
+++ src/main/java/entry/EntryPoint.java
@@ -1,7 +1,6 @@
 package entry;
 
 import java.lang.instrument.Instrumentation;
-import java.lang.reflect.InvocationTargetException;
 import java.util.LinkedHashMap;
 
 import org.javatuples.Triplet;
@@ -9,24 +8,35 @@ import org.javatuples.Triplet;
 import de.pfannekuchen.examplemod.Keybind;
 import de.pfannekuchen.mcloader.Mod;
 
+/**
+ * The Entry-Point of the Mod
+ * 
+ * This Class is forced to be in package entry with this Name!
+ * @author Pancake
+ */
 public class EntryPoint implements Mod {
 	
-	public Keybind exampleKeybind;
-	
+	/**
+	 * Create a new Keybind and display a Message in the Console of Minecraft.
+	 */
 	@Override
 	public void onInitialization(String args, Instrumentation instrumentation) {
-		System.out.println("Hello from the Minecraft Log!");
+		System.out.println("Hello from the Minecraft Console!"); // This Message will be printed in the Minecraft Console
 		try {
-			exampleKeybind = new Keybind("Example Keybinding", 0x22, "key.categories.misc");
-		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | SecurityException | ClassNotFoundException | NoSuchFieldException e) {
+			new Keybind("Example Keybinding", 0x22, "key.categories.misc");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * Print a Message every Tick
+	 */
 	@Override
 	public LinkedHashMap<Triplet<Class<?>, String, Boolean>, Runnable> getCallbacks(LinkedHashMap<Triplet<Class<?>, String, Boolean>, Runnable> callbacks) throws Exception {
+		// Create a Callback everytime runTick(t) is being called in Minecraft(bib)
 		callbacks.put(Triplet.with(Class.forName("bib"), "t", true), () -> {
-			System.out.println("Hello from the Minecraft Tick!");
+			System.out.println("Hello from the Minecraft Tick!"); // Print a Message everytime the Method is being executed.
 		});
 		return callbacks;
 	}
